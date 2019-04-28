98c66400f88cdd729b7fb20a3305308932fbf0af
chancheukkitjeffrey <49069626+chancheukkitjeffrey@users.noreply.github.com>
2019-04-28 22:35:17 +0800

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -1,16 +1,6 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
 
-/* -Member Vars of the employee:
-    -Employee ID
-    -Name
-    -Age
-    -Adress ***
-    -Role
-    -Sallary
-
-*/
-
 #include <iostream>
 #include <string>
 
@@ -22,63 +12,30 @@ private:
   string name;
   int age;
   string role;
-  int sallary;
-    
+  int salary;
+
 public:
   employee ();
   //default connstructor
-  
+
   ~employee ();
   //destructor
-  
+
   //"Set" functions. Will take the parameter passed in and set the member variable equal to it
   void setID (int);
   void setName (string);
   void setAge (int);
   void setRole (string);
-  void setSallary (int);
-  
-  //"Get" functions. Will return the member variable 
+  void setSalary (int);
+
+  //"Get" functions. Will return the member variable
   int getID ();
   string getName ();
   int getAge ();
   string getRole ();
-  int getSallary ();
-  
-	class address {
-	private:
-	  string street;
-	  string city;
-	  string state;
-	  string zip;
-	
-	public:
-		address();
-		//default const
-		
-		~address();
-		//destruct
-		
-		address(string, string, string, int);
-		//constructor with mem vars
-		
-		void setStreet(string);
-		void setCity(string);
-		void setState(string);
-		void setZip(string);
-		
-		string getStreet();
-		string getCity();
-		string getState();
-		string getZip();
-
-};
-    address addr;
+  int getSalary ();
 
 };
 
 
 #endif
-
-
-
